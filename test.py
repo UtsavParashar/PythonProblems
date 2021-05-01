61f1db0145f6d72a254fdc7a246a1d523e5cfba7
utsav <utsav.et@gmail.com>
2021-05-01 19:50:49 +0530

    Date range, bdate_range, period_range

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
-"""A,B,C
-1129,19-APR-2021,'Zip Code Details: City: Huntsville_Alabama , Zip: 35808 , 801thru816 City: Anchorage_Alaska , Zip: 99506 , 501thru524'
-1139,20-APR-2021,'Zip Code Details: City: Miami_Florida , Zip: 33128 , 124thru190 City: Atlanta_Georgia , Zip: 30301 , 301thru381'"""
+	sales
+place	
+8.284985e+06
+P	2.026861e+07
+Q	3.851017e+06
