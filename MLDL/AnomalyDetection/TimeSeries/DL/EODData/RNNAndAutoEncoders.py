6a6f49f3624f0ed04a381538f4be153648f91e58
utsav <utsav.et@gmail.com>
2023-07-16 21:34:31 +0530

    Test RNN on 2D TS Data

--- MLDL/AnomalyDetection/TimeSeries/DL/EODData/RNNAndAutoEncoders.py
+++ MLDL/AnomalyDetection/TimeSeries/DL/EODData/RNNAndAutoEncoders.py
@@ -7,19 +7,20 @@ def generate_synthetic_data(real_data):
   synthetic_data = tf.random.normal([1000, 2], mean=mean, stddev=stddev)
   return synthetic_data
 
-def train_anomaly_detection_model(synthetic_data):
-  """Trains a machine learning model to identify anomalies in the synthetic data."""
-  model = tf.keras.Sequential([
+def train_discriminator(real_data, synthetic_data):
+  """Trains the discriminator to distinguish between real and synthetic data."""
+  discriminator = tf.keras.Sequential([
       tf.keras.layers.Dense(128, activation='relu'),
       tf.keras.layers.Dense(1, activation='sigmoid')
   ])
-  model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-  model.fit(synthetic_data, epochs=10)
-  return model
+  discriminator.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+  discriminator.fit(real_data, tf.ones([len(real_data)], dtype=tf.int32), epochs=10)
+  discriminator.fit(synthetic_data, tf.zeros([len(synthetic_data)], dtype=tf.int32), epochs=10)
+  return discriminator
 
-def detect_anomalies(real_data, model):
-  """Detects anomalies in the real data using the trained model."""
-  predictions = model.predict(real_data)
+def detect_anomalies(real_data, discriminator):
+  """Detects anomalies in the real data using the trained discriminator."""
+  predictions = discriminator.predict(real_data)
   anomalies = []
   for i, prediction in enumerate(predictions):
     if prediction < 0.5:
@@ -27,14 +28,21 @@ def detect_anomalies(real_data, model):
   return anomalies
 
 def main():
-  """Generates synthetic data, trains a machine learning model, and detects anomalies."""
-  real_data = tf.random.normal([1000, 2])
-  # The first dimension is the date and the second dimension is the int type.
+  """Finds anomalies on 2D timeseries data using TGAN-AD method."""
+  # Load the real data.
+  real_data = tf.load_data("real_data.csv")
 
+  # Generate synthetic data.
   synthetic_data = generate_synthetic_data(real_data)
-  model = train_anomaly_detection_model(synthetic_data)
-  anomalies = detect_anomalies(real_data, model)
+
+  # Train the discriminator.
+  discriminator = train_discriminator(real_data, synthetic_data)
+
+  # Detect anomalies.
+  anomalies = detect_anomalies(real_data, discriminator)
+
+  # Print the anomalies.
   print(anomalies)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
   main()
