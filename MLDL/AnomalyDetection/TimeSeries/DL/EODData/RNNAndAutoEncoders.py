2461ed0508be5d1bf9e52e2aaf28edf22a90b667
utsav <utsav.et@gmail.com>
2023-07-16 17:38:22 +0530

    Test RNN on 2D TS Data

--- MLDL/AnomalyDetection/TimeSeries/DL/EODData/RNNAndAutoEncoders.py
+++ MLDL/AnomalyDetection/TimeSeries/DL/EODData/RNNAndAutoEncoders.py
@@ -1,81 +1,39 @@
-import numpy as np
-import pandas as pd
 import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
+import tensorflow_probability as tfp
 
-# Load and preprocess your two-dimensional time series dataset
-# Assuming your dataset is in a pandas DataFrame with columns 'date' and 'value'
-data = pd.read_csv('your_dataset.csv')
-dates = pd.to_datetime(data['date'])
-values = data['value']
+def generate_synthetic_data(real_data):
+  """Generates synthetic data that is similar to the real data."""
+  model = tfp.generative_models.GaussianProcessRegressionModel(
+      train_points=real_data)
+  synthetic_data = model.sample(1000)
+  return synthetic_data
 
-# Normalize the values
-normalized_values = (values - np.mean(values)) / np.std(values)
+def train_anomaly_detection_model(synthetic_data):
+  """Trains a machine learning model to identify anomalies in the synthetic data."""
+  model = tf.keras.Sequential([
+      tf.keras.layers.Dense(128, activation='relu'),
+      tf.keras.layers.Dense(1, activation='sigmoid')
+  ])
+  model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+  model.fit(synthetic_data, epochs=10)
+  return model
 
-# Prepare input sequences for the RNN
-sequence_length = 10  # Number of previous time steps to consider for each data point
-input_sequences = []
-for i in range(sequence_length, len(normalized_values)):
-    input_sequences.append(normalized_values[i - sequence_length: i + 1])
-input_sequences = np.array(input_sequences)
+def detect_anomalies(real_data, model):
+  """Detects anomalies in the real data using the trained model."""
+  predictions = model.predict(real_data)
+  anomalies = []
+  for i, prediction in enumerate(predictions):
+    if prediction < 0.5:
+      anomalies.append(i)
+  return anomalies
 
-# Split the data into training and validation sets
-train_ratio = 0.8
-train_size = int(train_ratio * len(input_sequences))
-train_data = input_sequences[:train_size]
-val_data = input_sequences[train_size:]
+def main():
+  """Generates synthetic data, trains a machine learning model, and detects anomalies."""
+  real_data = tf.random.normal([1000, 1])
+  synthetic_data = generate_synthetic_data(real_data)
+  model = train_anomaly_detection_model(synthetic_data)
+  anomalies = detect_anomalies(real_data, model)
+  print(anomalies)
 
-# Reshape the data to match the expected input shape of the model
-train_data = np.reshape(train_data, (train_data.shape[0], train_data.shape[1], 1))
-val_data = np.reshape(val_data, (val_data.shape[0], val_data.shape[1], 1))
-
-# Define the RNN-Autoencoder model
-latent_dim = 10
-
-# Encoder
-encoder_inputs = keras.Input(shape=(sequence_length + 1, 1))
-encoder = layers.LSTM(latent_dim, return_sequences=False)(encoder_inputs)
-
-# Decoder
-decoder_outputs = layers.RepeatVector(sequence_length + 1)(encoder)
-decoder_outputs = layers.LSTM(latent_dim, return_sequences=True)(decoder_outputs)
-decoder_outputs = layers.TimeDistributed(layers.Dense(1))(decoder_outputs)
-
-# Define the RNN-Autoencoder model
-model = keras.Model(encoder_inputs, decoder_outputs)
-
-# Compile the model
-model.compile(optimizer="adam", loss="mse")
-
-# Train the model
-model.fit(train_data, train_data, validation_data=(val_data, val_data), epochs=50, batch_size=16)
-
-# Reconstruct the training data using the trained model
-reconstructed_train_data = model.predict(train_data)
-
-# Calculate reconstruction error (MSE) between original and reconstructed data
-train_mse = np.mean(np.power(train_data - reconstructed_train_data, 2), axis=1)
-
-# Set a threshold for anomaly detection
-threshold = 0.1  # Adjust the threshold based on your dataset and requirements
-
-# Identify anomalies in the training data
-train_anomalies = dates[sequence_length:train_size][train_mse > threshold]
-
-# Print the detected anomalies in the training data
-print("Detected anomalies in the training data:")
-print(train_anomalies)
-
-# Reconstruct the validation data using the trained model
-reconstructed_val_data = model.predict(val_data)
-
-# Calculate reconstruction error (MSE) between original and reconstructed data
-val_mse = np.mean(np.power(val_data - reconstructed_val_data, 2), axis=1)
-
-# Identify anomalies in the validation data
-val_anomalies = dates[train_size + sequence_length:][val_mse > threshold]
-
-# Print the detected anomalies in the validation data
-print("Detected anomalies in the validation data:")
-print(val_anomalies)
+if __name__ == '__main__':
+  main()
